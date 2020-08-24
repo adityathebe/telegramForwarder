e416b567fd8e39a326d5060fae0ddb56e5c003bf
Aditya <thebeaditya@gmail.com>
2020-08-24 15:25:39 +0545

    Upgraded NPM packages
    - Removed unncessary packages

--- bot/routes/agent.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  console.log('Agent Route');
-  res.send('Agent Route');
-});
-
-module.exports = router;
--- bot/routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get("/", (req, res) => {
-  console.log('Root Page')
-  res.send('Root Page')
-});
-
-module.exports = router;
